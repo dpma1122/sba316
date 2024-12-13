fee073bbbc0f5116f2e292f234d09060fea256cc
dpma1122 <dpma1122@yahoo.com>
2024-12-13 14:56:48 -0500

    element1

---
+++ app.js
@@ -0,0 +1,6 @@
+ const category1 = document.querySelector(".button1").style.color = "red";
+
+
+const category2 = document.querySelector (".button2").style.color = "red"
+//cache at least one element
+
