5f0f698daf162bc218b719dd10f7bf11994a8c04
dpma1122 <dpma1122@yahoo.com>
2024-12-14 06:55:02 -0500

    game

--- app.js
+++ app.js
@@ -4,3 +4,8 @@
 const category2 = document.querySelector (".button2").style.color = "red"
 //cache at least one element
 
+let button1 = ["Mexican", "Chinese", "Japanese", "Greek"]
+
+let button2= ["Mexico", "China", "Japan", "Greece"]
+
+//  use iteration (repeat instructions)
